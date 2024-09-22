cf81103e50e42209b0bb3d4c422be2d0ff99b843
vinic <vinic@host.docker.internal>
2024-09-22 17:15:13 -0300

    first step

---
+++ 05_firstSteps/rest-with-spring-boot-java-erudio/src/test/java/com/java/erudio/StartupTests.java
@@ -0,0 +1,13 @@
+package com.java.erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
